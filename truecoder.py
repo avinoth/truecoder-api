d161caa997db61ea792c28f84548d79b9d817e47
Vinoth <a.vinothk@outlook.com>
2015-08-25 08:44:59 +0530

    add api calls

--- truecoder.py
+++ truecoder.py
@@ -1,10 +1,19 @@
 import webapp2
+from google.appengine.api import urlfetch
+import json
 
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('welcome!')
+  def get(self):
+    url = "https://api.github.com/users/avinoth/repos"
+    result = urlfetch.fetch(url)
+    if result.status_code != 200:
+      data = {'status': 'Error from Github - ' + str(result.status_code)}
+    else:
+      payload = json.loads(result.content)
+      data = payload[0]['name']
+    self.response.headers['Content-Type'] = 'application/json'
+    self.response.write(data)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+  ('/', MainPage),
 ], debug=True)
